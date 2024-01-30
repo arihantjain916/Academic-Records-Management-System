bb10f73428e12eb1ee30a10519a0d753bf93a3e3
Arihant Jain <81769948+arihantjain916@users.noreply.github.com>
2024-01-30 17:45:34 +0530

    Delete ERP-Portal Backend directory

--- ERP-Portal Backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-const generateToken = (id, type) => {
-    return jwt.sign({ id, type }, process.env.JWT_SECRET, {
-        expiresIn: '30d'
-    })
-}
-
-module.exports = generateToken
